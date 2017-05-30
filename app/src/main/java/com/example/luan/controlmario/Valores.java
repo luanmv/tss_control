e23b109bc15e1e3d09d2203d79e856e8e29a6dd8
Luis <mtzvaldezluis@outlook.com>
2017-05-30 18:04:11 -0500

    values

--- app/src/main/java/com/example/luan/controlmario/Valores.java
+++ app/src/main/java/com/example/luan/controlmario/Valores.java
@@ -4,10 +4,10 @@ package com.example.luan.controlmario;
  * Created by Luís on 30/05/2017.
  */
 
-public class Valores {
+class Valores {
 
-    public static int InicialX = 0;
-    public static int InicialY = 0;
-    public static int InicialZ = 0;
+    static int InicialX = 0;
+    static int InicialY = 0;
+    static int InicialZ = 0;
 
 }
