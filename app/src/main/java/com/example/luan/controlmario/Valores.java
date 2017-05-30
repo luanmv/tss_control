f431a3bcd90450776c1d0a022ffe6a4f265afed1
Luis <mtzvaldezluis@outlook.com>
2017-05-30 18:03:47 -0500

    values

---
+++ app/src/main/java/com/example/luan/controlmario/Valores.java
@@ -0,0 +1,13 @@
+package com.example.luan.controlmario;
+
+/**
+ * Created by Luís on 30/05/2017.
+ */
+
+public class Valores {
+
+    public static int InicialX = 0;
+    public static int InicialY = 0;
+    public static int InicialZ = 0;
+
+}
